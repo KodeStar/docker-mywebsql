d33d02fad2b9e89bc85db37517b31c81dc157a35
sparklyballs <sparklyballs@gmail.com>
2015-08-07 12:33:51 +0100

    Update 80_set_up_dbase.sh

--- init/80_set_up_dbase.sh
+++ init/80_set_up_dbase.sh
@@ -2,6 +2,7 @@
 if [ ! -d "$DATADIR/mysql" ]; then
 echo "Setting Up Initial Databases"
 mkdir -p "$DATADIR" /config/log/mariadb /var/run/mysqld
+chmod -R 777 /var/run/mysqld
 mysql_install_db --datadir="$DATADIR" >/dev/null 2>&1
 
 tempSqlFile='/tmp/mysql-first-time.sql'
@@ -12,7 +13,7 @@ GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
 EOSQL
-mysqld --init-file="$tempSqlFile" >/dev/null 2>&1
+mysqld --init-file="$tempSqlFile" >/dev/null 2>&1 &
 chown -R abc:abc "$MARIADB_DIR" "$DATADIR" /config/log/mariadb /var/run/mysqld
 echo "Database Setup Conpleted"
 fi
